068eb8dbba0912edfec0bb5601ccfb50cf4fae31
Thomas <w4pservice@gmail.com>
2017-02-22 11:29:29 +0100

    add Service

--- src/app/service/info.service.ts
+++ src/app/service/info.service.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable }              from '@angular/core';
+import { Http, Response }          from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { DockerInfo } from '../model/docker.info';
+
 
 @Injectable()
 export class InfoService {
+
+  private heroesUrl = 'app/heroes';  // URL to web API
+
+  constructor (private http: Http) {}
+
+  getHeroes (): Observable<DockerInfo> {
+    return this.http.get(this.heroesUrl)
+                    .map((res:Response) => res.json())
+                    .catch(this.handleError);
+  }
+  
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
