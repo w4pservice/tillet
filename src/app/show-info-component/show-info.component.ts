1d84f91eac33a2b5c105eb279f085a2ffb336dbe
Thomas Keller <w4pservice@gmail.com>
2017-04-09 19:00:53 +0200

    new info page

--- src/app/show-info-component/show-info.component.ts
+++ src/app/show-info-component/show-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoService } from '../service/info.service';
+import { AppManager } from '../service/app-manager.service';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -13,12 +14,18 @@ export class ShowInfo implements OnInit {
   info:   string;
   error:  string;
 
-  constructor( private infoService: InfoService ) { }
+  constructor( private infoService: InfoService, private manager: AppManager ) { }
 
   ngOnInit() : void {
     this.infoService.getInfo().subscribe(
-      info => this.info = info,
-      error => this.error = error
+      info => {
+        this.info = info;
+        this.manager.setConnected(true);
+      },
+      error => {
+        this.error = error;
+        this.manager.setConnected(false);
+      }
     );
   }
 }
